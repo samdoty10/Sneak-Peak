9e633574413cf4508b4204574fb2d30dcdbd0a79
samdoty10 <samdoty208@gmail.com>
2026-05-06 10:43:46 -0500

    5/5/26

--- src/App.tsx
+++ src/App.tsx
@@ -10,15 +10,28 @@ import {
   Avatar,
   Badge,
 } from "@chakra-ui/react";
-import tweetsData from "./data/tweets.json";
 import type { Tweet } from "./types/Tweet";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { supabase } from "./utils/supabase";
 
 function App() {
   // Tweets is the current list of tweets shown
   // setTweets is how React updates whats shows
   // We start with tweets from our json file
-  const [tweets, setTweets] = useState<Tweet[]>(tweetsData as Tweet[])
+  const [tweets, setTweets] = useState<Tweet[]>([])
+  useEffect(() => {
+  async function load() {
+    const { data, error } = await supabase
+      .from("tweets")
+      .select("*")
+      .order("created_at", { ascending: false });
+
+    if (error) console.error(error);
+    else setTweets(data || []);
+  }
+
+  load();
+}, []);
   
   // input is what is currently typed in the box
   //setInput is how react knows about newly typed data
